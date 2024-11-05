e046dccabecd9e285605d6af9a36924b773d4985
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-11-05 17:36:13 +0530

    refactor: restructure Header component for improved layout and add navigation link to project creation

--- components/header.jsx
+++ components/header.jsx
@@ -2,25 +2,29 @@ import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@c
 import Image from "next/image"
 import Link from "next/link"
 
-const Header =()=>{
+const Header = () => {
     return (
-    <header className="container mx-auto">
-    <nav className="py-6 px-4 flex justify-between items-center ">
-        <Link href='/'>
-             <Image src ={'/logo.svg'} alt="ziraclone Logo" width={200} height={56}
-             className="h-10 w-auto object-contain"
-             />
-        </Link>
-
-        <SignedIn>
-            <UserButton />
-            <SignOutButton />
-        </SignedIn>
-        <SignedOut>
-            <SignInButton />
-        </SignedOut>
-        </nav>
-    </header>
+        <header className="container mx-auto">
+            <nav className="py-6 px-4 flex justify-between items-center ">
+                <Link href='/'>
+                    <Image src={'/logo.svg'} alt="ziraclone Logo" width={200} height={56}
+                        className="h-10 w-auto object-contain"
+                    />
+                </Link>
+            <div>
+                <Link href='/project/create'>
+                    <a className="text-gray-800 hover:text-gray-600">Home</a>
+                </Link>
+                <SignedIn>
+                    <UserButton />
+                    <SignOutButton />
+                </SignedIn>
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+            </div>
+            </nav>
+        </header>
     )
 }
 export default Header
