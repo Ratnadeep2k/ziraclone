056b9d2be133387214fb683f3ccfdb93aaa853b2
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-11-06 13:07:38 +0530

    feat: enhance header component with navigation and logo

--- components/header.jsx
+++ components/header.jsx
@@ -1,18 +1,40 @@
 import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button'
+import { PenBox } from 'lucide-react'
 
 const Header = () => {
   return (
-    <div>
-      <SignedOut>
-         <SignInButton/>      
-      </SignedOut>
-      <SignedIn>
-        <p>Hi, you are signed in!</p>
-        <UserButton/>
-        <SignOutButton/>
-      </SignedIn>
-    </div>
+    <header className='container mx-auto'>
+
+      <nav className='py-6 px-4 flex justify-between items-center'>
+        <Link href='/'>
+          <Image src="/logo.svg" alt="Logo" width={200} height={56}
+            className='h-10 w-auto object-contain'
+          />
+        </Link>
+        <div className='flex items-center gap-4'>
+          <Link href='/project/create'>
+            <Button variant ="destructive" className="flex items-center gap-2">
+              <PenBox size={12}/>
+              Create Project
+            </Button>
+          </Link>
+          <SignedOut>
+            <SignInButton forceRedirectUrl='/onboarding'>
+                  <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <p>Hi, you are signed in!</p>
+            <UserButton />
+            <SignOutButton />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
   )
 }
 
