aa000f24fd2ee266cc979016d86df8b921b93d77
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-11-06 11:48:48 +0530

    feat: enhance UI components with new input, textarea, and badge components; add accordion, tabs, popover, and avatar components; update ESLint rules and Tailwind animations

--- app/page.js
+++ app/page.js
@@ -1,5 +1,12 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
- return <div>Home</div>
+ return (
+    <div>
+      <Button>Click me</Button>
+      Home
+  </div>
+
+
+)
 }
