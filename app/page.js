57e84a3c9d30cdf5b3d4982afe1b84382907f71b
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-11-10 17:14:57 +0530

    feat: add dotted background style and enhance homepage with hero section and call-to-action

--- app/page.js
+++ app/page.js
@@ -1,9 +1,33 @@
 import { Button } from "@/components/ui/button";
+import { ChevronRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
  return (
     <div>
-    <h1>Hello World</h1>
+      {/* HeroSection */}
+      <section>
+        <h1>Streamline your workflow
+          <br />
+         <span>
+            with{""}
+            <Image
+            src={'/logo.svg'}
+            alt="Zira"
+            width={400}
+            height={80}
+            className="h-14 sm:h-24 w-auto object-contain"
+            />
+         </span>
+        </h1>
+        <p>Manage your projects with ease</p>
+        <Link href='/onboarding'>
+          <Button  size='lg' className='mr-4'>
+            Get Started  <ChevronRight size={18} className="ml-1"/>
+          </Button>
+        </Link>
+      </section>
   </div>
 
 
