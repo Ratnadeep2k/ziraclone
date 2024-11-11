ef211a0dba11232897c1e3c6cd2663c3c240145c
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-11-11 14:13:43 +0530

    feat: update homepage layout with new styles and buttons; replace logo and enhance header navigation

--- app/page.js
+++ app/page.js
@@ -7,33 +7,44 @@ export default function Home() {
  return (
     <div className="min-h-secreen">
       {/* HeroSection */}
-      <section className="container mx-auto py-20 text-center">
-        <h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold gradient-title pb-6 flex felx-col">
+      <section className="container mx-auto min-h-screen flex items-center justify-center">
+      <div className="text-center space-y-8">
+        <h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold gradient-title">
           Streamline your workflow
           <br />
-         <span>
-            with
+          <span className="flex mx-auto gap-3 sm:gap-4 items-center justify-center mt-4">
+            with{" "}
             <Image
-            src={'/logo.svg'}
-            alt="Zira"
-            width={400}
-            height={80}
-            className="h-14 sm:h-24 w-auto object-contain"
+              src="/logo2.png"
+              alt="Zira"
+              width={400}
+              height={80}
+              className="h-14 sm:h-24 w-auto object-contain"
             />
-         </span>
+          </span>
         </h1>
-        <p>Manage your projects with ease</p>
-        <Link href='/onboarding'>
-          <Button  size='lg' className='mr-4'>
-            Get Started  <ChevronRight size={18} className="ml-1"/>
-          </Button>
-        </Link>
-        <Link href='#features'>
-          <Button varient='outline' size='lg' className='mr-4'>
-            Learn More <ChevronRight size={18} className="ml-1"/>
-          </Button>
-        </Link>
-      </section>
+        
+        <p className="text-lg sm:text-xl">
+          Manage your projects with ease
+        </p>
+        
+        <div className="flex justify-center gap-4 pt-4">
+          <Link href="/onboarding">
+            <Button size="lg" className="mr-4">
+              Get Started
+              <ChevronRight size={18} className="ml-1" />
+            </Button>
+          </Link>
+          
+          <Link href="#features">
+            <Button variant="outline" size="lg" className="mr-4">
+              Learn More
+              <ChevronRight size={18} className="ml-1" />
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </section>
   </div>
 
 
