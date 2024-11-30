07e5c84bd0c26859356b95174d09a06b35ef5fca
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-11-30 18:46:42 +0530

    feat: enhance getOrganization action with organization membership validation

--- actions/organization.js
+++ actions/organization.js
@@ -1,7 +1,7 @@
 'use server'
 
 import { db } from "@/lib/prisma";
-import { auth } from "@clerk/nextjs/dist/types/server";
+import { auth, clerkClient } from "@clerk/nextjs/dist/types/server";
 
 export async function  getOrganization(slug){
     const {userId} =auth();
@@ -19,6 +19,21 @@ export async function  getOrganization(slug){
         throw new Error('User not exist');
     }
 
-    
+    const organization = await clerkClient().organizations.getOrganization({
+        slug
+    });
+    if(!organization){
+        throw new Error('Organization not exist');
+    }
+
+    const {data : membership } = await clerkClient().organizations.getOrganizationMembershipList({
+        organizationId: organization.id
+    });
+
+    const userMembership = membership.find((memeber)=> memeber.publicUserData.userId === userId)
+    if(!userMembership){
+        throw new Error('User not a member of this organization');
+    }
+    return organization;
 
 }
