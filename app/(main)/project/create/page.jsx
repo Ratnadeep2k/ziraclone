55f4d55264bbb0d3b19f46b17c29a3a4a5019fa0
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-12-08 14:00:25 +0530

    feat: implement admin check for project creation and add organization switcher

--- app/(main)/project/create/page.jsx
+++ app/(main)/project/create/page.jsx
@@ -1,6 +1,31 @@
 'use client'
-import React from 'react'
+import { useOrganization, useUser } from '@clerk/nextjs';
+import React, { useEffect, useState } from 'react'
+import OrgSwitcher from '@/components/org-switcher'
 const CreateProjectPage = () => {
+
+    const {isLoaded : isOrgLoaded ,membership} = useOrganization();
+    const {isLoaded: isUserLoaded} = useUser();
+    const {isAdmin ,setIsAdmin} = useState(false);
+
+    useEffect(()=>{
+        if(isOrgLoaded && isUserLoaded && membership){
+           setIsAdmin(membership.role === 'org:admin');
+        }
+    },[isOrgLoaded, isUserLoaded, membership])
+
+    if(!isOrgLoaded || !isUserLoaded){
+        return null;
+    }
+    if(!isAdmin){
+        return <div>
+            <span>"Only Admin can create Projects</span>\
+            <OrgSwitcher/>
+        </div>
+
+    }
+
+
   return (
     <div>page</div>
   )
