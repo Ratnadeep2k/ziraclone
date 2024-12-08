d92e4a3d66989a2771c009a3aa3551ea1db039a2
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-12-08 13:44:22 +0530

    feat: enhance createProject action with organization membership validation and authorization checks

--- actions/projects.js
+++ actions/projects.js
@@ -1,3 +1,4 @@
+import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/dist/types/server";
 
 export async function createProject(data){
@@ -7,5 +8,31 @@ export async function createProject(data){
     }
     if(!orgId){
         throw new Error('Organization not found')
+    }
+    const {data : membership } = await clerkClient().organizations.getOrganizationMembershipList({
+        organizationId: organization.id
+    });
+
+    const userMembership = membership.find((memeber)=> memeber.publicUserData.userId === userId)
+    if(!userMembership){
+        throw new Error('User not a member of this organization');
+    }
+
+    if(!userMembership || !userMembership.role || userMembership.role !== 'org:admin'){
+        throw new Error('User not authorized to create project');
+    }
+    try{
+        const project = await db.project.createProject({
+            data:{
+               name : data.name,
+               key :data.key,
+               description : data.description,
+               organizationId : orgId,
+            }
+        })
+        return project;
+    }
+    catch(e){
+        throw new Error('Failed to create project');
     }
 }
