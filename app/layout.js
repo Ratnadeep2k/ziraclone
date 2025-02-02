8cef11acb6a42bff5fa019681cb4623b65098ee3
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-02-02 15:02:10 +0530

    feat: refactor layout and theme provider components for improved structure and readability

--- app/layout.js
+++ app/layout.js
@@ -1,43 +1,44 @@
+
 import Header from "@/components/header";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Inter } from "next/font/google"
 import { ClerkProvider } from "@clerk/nextjs";
 import { shadesOfPurple } from "@clerk/themes";
-const inter =Inter({subsets:["latin"]})
+const inter = Inter({ subsets: ["latin"] })
 export const metadata = {
   title: "Zira clone",
   description: "A Project management application for teams",
 };
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider 
-    appearance={{
-      baseTheme: shadesOfPurple,
-    }}
-    
-    >
-    <html lang="en">
+
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} dotted-background`}
       >
-        <ThemeProvider attribute ="class" defaultTheme ='dark'>
-          {/* Header */}
-          <Header/>
-          <main className="min-h-screen"> 
-          {children}
-          </main>
-        
-          {/* Footer */}
-          <footer className="bg-gray-900 py-12" >
-            <div className="container mx-auto text-center text-gray-200">
-              <p>&copy; 2024 Zira Clone</p>
-            </div>
-          </footer>
-        </ThemeProvider>
-        
+        <ClerkProvider
+          appearance={{
+            baseTheme: shadesOfPurple,
+          }}
+
+        >
+          <ThemeProvider attribute="class" defaultTheme='dark'>
+            {/* Header */}
+            <Header />
+            <main className="min-h-screen">
+              {children}
+            </main>
+
+            {/* Footer */}
+            <footer className="bg-gray-900 py-12" >
+              <div className="container mx-auto text-center text-gray-200">
+                <p>&copy; 2024 Zira Clone</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </ClerkProvider>
       </body>
     </html>
-    </ClerkProvider>
   );
 }
